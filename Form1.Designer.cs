71fbb81b2cd62edfbd0621e1d68591b090ec6330
Andrzej Olszak <andrzej@alipescapital.com>
2018-01-26 18:28:43 +0100

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace PerfMonViewerDeluxe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this._overviewChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this._overviewChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _overviewChart
+            // 
+            this._overviewChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.MajorGrid.Interval = 3600D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.DimGray;
+            chartArea1.AxisX.MajorTickMark.Interval = 3600D;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.DimGray;
+            chartArea1.AxisX.MinorGrid.Enabled = true;
+            chartArea1.AxisX.MinorGrid.Interval = 900D;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisX.MinorTickMark.Interval = 900D;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.LabelStyle.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.DimGray;
+            chartArea1.Name = "ChartArea1";
+            this._overviewChart.ChartAreas.Add(chartArea1);
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Row;
+            legend1.Name = "Legend1";
+            this._overviewChart.Legends.Add(legend1);
+            this._overviewChart.Location = new System.Drawing.Point(2, 41);
+            this._overviewChart.Name = "_overviewChart";
+            this._overviewChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            this._overviewChart.Size = new System.Drawing.Size(1067, 678);
+            this._overviewChart.TabIndex = 4;
+            this._overviewChart.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Load a .blg file...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(1075, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(266, 325);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1353, 731);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this._overviewChart);
+            this.Name = "Form1";
+            this.Text = "PerMon Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this._overviewChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart _overviewChart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Charting;
+using System.Reactive;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Humanizer;
+using Tx.Windows;
+
+namespace PerfMonViewerDeluxe
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<PerformanceSample>> _performanceData;
+        private Dictionary<string, FastLine> _trends = new Dictionary<string, FastLine>();
+        private DateTime _start = new DateTime(2999, 12, 12);
+        private DateTime _end = new DateTime(1024, 12, 12);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetupZoomInteractions(this._overviewChart);
+            SetupDefaultVisualStyles(this._overviewChart);
+            _overviewChart.FormatNumber += FormatNumber;
+        }
+
+        public static void SetupZoomInteractions(System.Windows.Forms.DataVisualization.Charting.Chart targetChart)
+        {
+            foreach (System.Windows.Forms.DataVisualization.Charting.ChartArea area in targetChart.ChartAreas)
+            {
+                area.CursorX.IsUserEnabled = true;
+                area.CursorX.IsUserSelectionEnabled = true;
+                area.AxisX.ScaleView.Zoomable = true;
+                area.AxisX.ScrollBar.IsPositionedInside = true;
+
+                targetChart.MouseClick += (s, e) =>
+                {
+                    if (e.Button == MouseButtons.Right)
+                    {
+                        area.AxisX.ScaleView.ZoomReset();
+                    }
+                };
+            }
+        }
+
+        public static void SetupDefaultVisualStyles(System.Windows.Forms.DataVisualization.Charting.Chart targetChart)
+        {
+            foreach (System.Windows.Forms.DataVisualization.Charting.ChartArea area in targetChart.ChartAreas)
+            {
+                area.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+                area.AxisX.MinorGrid.LineColor = System.Drawing.Color.Gainsboro;
+                area.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+                area.AxisY.MinorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            }
+        }
+
+        private void FormatNumber(object sender, FormatNumberEventArgs e)
+        {
+            e.LocalizedValue = e.Value.ToMetric();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            this.openFileDialog1.FileName = "*.blg";
+            this.openFileDialog1.Filter = "PerfMon Binary Logs|*.blg";
+            this.openFileDialog1.Multiselect = true;
+
+            DialogResult res = this.openFileDialog1.ShowDialog(this);
+
+            if (res == DialogResult.OK)
+            {
+                string fileName = this.openFileDialog1.FileName;
+
+                this._performanceData = new Dictionary<string, List<PerformanceSample>>();
+
+                var playback = new Playback();
+                playback.AddPerfCounterTraces(fileName);
+                playback.GetObservable<PerformanceSample>().Subscribe(StoreData);
+                playback.Run();
+
+                this.listBox1.Items.Clear();
+                foreach (string id in this._performanceData.Keys.OrderBy(x => x).ToList())
+                {
+                    this.listBox1.Items.Add(id);
+                }
+
+                this.Text = this._start.ToShortDateString() + " - " + this._end.ToShortDateString() + ": " + fileName;
+            }
+        }
+
+        private void StoreData(PerformanceSample sample)
+        {
+            string id = sample.CounterSet + "->" + sample.CounterName + "->" + sample.Instance;
+            List<PerformanceSample> list;
+            if (!this._performanceData.TryGetValue(id, out list))
+            {
+                list = new List<PerformanceSample>();
+                this._performanceData.Add(id, list);
+            }
+
+            list.Add(sample);
+
+            if (sample.Timestamp > this._end)
+            {
+                this._end = sample.Timestamp;
+            }
+
+            if (sample.Timestamp < this._start)
+            {
+                this._start = sample.Timestamp;
+            }
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            _overviewChart.Series.Clear();
+            this._trends.Clear();
+
+            double maxVal = 0;
+            foreach (object id in this.listBox1.SelectedItems)
+            {
+                string ids = id.ToString();
+                List<PerformanceSample> samples = this._performanceData[ids];
+
+                foreach (PerformanceSample s in samples)
+                {
+                    FastLine trend;
+                    if (!_trends.TryGetValue(ids, out trend))
+                    {
+                        trend = new FastLine { LegendText = ids };
+                        trend.BorderWidth = 3;
+                        _trends.Add(ids, trend);
+                        _overviewChart.Series.Add(trend);
+                    }
+
+                    trend.Add(s.Timestamp.ToLongTimeString(), new FastLine.DataPoint(s.Value));
+
+                    if (s.Value > maxVal)
+                    {
+                        maxVal = s.Value;
+                    }
+                }
+            }
+
+            //_overviewChart.ChartAreas[0].AxisX.ScaleView.ZoomReset();
+            _overviewChart.ChartAreas[0].AxisY.Maximum = maxVal;
+
+            _overviewChart.Invalidate();
+        }
+    }
+}
